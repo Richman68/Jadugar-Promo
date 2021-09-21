ce9e50df9c78334529451a136c92d2875410e534
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-21 09:46:03 +0530

    update config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 class Config:
     BOT_TOKEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
     sheetname = "promotionv4"
-    SHEETA = "data1"
+    sheetA = "channels"
+    sheetB = "users"
     app = "akhilpdisk"
     chnlid = "1551862526"
     CHANNEL_ID="-100" + chnlid
