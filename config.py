619ae05cb7ce5e677ae0baa7605dee7d5f877e4f
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-21 09:43:49 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 class Config:
     BOT_TOKEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
+    sheetname = "promotionv4"
     SHEETA = "data1"
     app = "akhilpdisk"
     chnlid = "1551862526"
