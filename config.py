21abd563b51c8034b3d07b06a0121989dc1e5eb8
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-21 09:48:42 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 class Config:
-    BOT_TOKEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
+    BOT_TOKEN = "2036153841:AAE7e6fRrv430Qoau0JsI-Mr27ElNB74hDA"
     sheetname = "promotionv4"
     sheetA = "channels"
     sheetB = "users"
-    app = "akhilpdisk"
+    app = "promotionv4"
     chnlid = "1551862526"
     CHANNEL_ID="-100" + chnlid
