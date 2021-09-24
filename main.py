f6e32e4c5a29b5fb1d4fc93fc7f89d04edae6fed
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-24 15:38:04 +0530

    update main.py

--- main.py
+++ main.py
@@ -74,7 +74,8 @@ def admincmd(m):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
   if call.data == "hlp":
-    bot.edit_message_text(chat_id = call.message.chat.id,text=f"{normaltext.HelpText}",message_id=call.message.id,reply_markup=buttons.HlpBtn.key,parse_mode="HTML")
+    bot.send_message(call.messg)
+    bot.edit_message_text(chat_id = call.message.chat.id,text=f"{normaltext.HelpText}",message_id=call.message.message_id,reply_markup=buttons.HlpBtn.key,parse_mode="HTML")
   if call.data == "backtohome":
     usrlnk = f"<a href='tg://user?id={call.message.chat.id}'>{call.from_user.first_name}</a>"
     bot.edit_message_text(chat_id = call.message.chat.id,text=f"<b>{normaltext.welcome.format(usrlnk)}</b>",message_id=call.message.id,reply_markup=buttons.Wlcmbtn.key,parse_mode="HTML")
