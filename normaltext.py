ea77a1f461ec5c280a5090abf90bd66db38add7d
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-24 14:59:24 +0530

    update normaltext.py

--- normaltext.py
+++ normaltext.py
@@ -1,5 +1,48 @@
-import os
-class Welcome:
-    welcome = '''kya kr rha b ...
-    vhaa kb jayega 
-    kya krega jakr'''
+welcome = '''👋 Welcome {} . 
+This is Promotion Bot for Registration 
+Of channels.'''
+
+adminpnl = '''👤 This is The Admin Pannel'''
+
+nonadminpnl = '''<b>Opps! ☹️ , You are not an admin</b>'''
+
+sharetext = '''<b>I invite You to join The Best ProkutPromotion Bot of Ever</b>'''
+
+AdminUsername = "@OfficialManagerAdmin"
+MaxLimitUser = 5
+botUsername = "Promotionv4TestBot"
+
+HelpText = '''<b>⚠️ This is Help Pannel:
+
+This Is A Promotion Bot. If u Need any Type of Help about promoting of Your Channel. Your can frequently ask your questions directly Just Ping Me @Jai_Mahakal_Ji .
+
+◼️Promotion Rules
+───────────
+➤ 24 hours in the channel.
+➤ Don't Delete The List Before 24 hours Manually.
+
+● If you want to get unban then contact Admin
+
+🚫 Those who break the rules will be banned from the bot.</b>'''
+
+dvlprText = '''<b>About Me 😎
+🤖 Name :''' + "<a href='t.me/{}'>{}</a>" + '''
+👨‍💻 Developer : @Jai_Mahakal_Ji
+📝 Language : </b><code>Python3.8</code>
+<b>🧰 Framework :</b> <code>PyTelegramBotAPI</code>
+<b>📡 Server :</b> <code>Heroku</code>
+<b>Made In India 🇮🇳 ❤️</b>'''
+
+note = "⚠️ Note:- Your default channel description is your channel name. If u want to update it then use comamnd"
+ChnlAdSucess = f"<b>✅ Your channel</b> "+ '<a href="{}">{} </a>' + f"<b>is added successfully. </b> \n\n<code>{note}</code> /update_Chnl_Data ."
+
+ReisterStepA = '''<b>Now follow the following steps</b>
+
+<code>1. Add bot as Admin with Post and Delete Rights.
+2. Forward any Parent message from the respective channel</code>'''
+
+NotanyChnl = '''<b>⚠️ You haven't registered any channel with our bot yet Or Channels might have been removed or banned</b>'''
+
+NotEnfSub = "<b>Error:</b> <code>Your Channel Have not Minimum Required Subscribers. Should Have minimum" + f" {MaxLimitUser} " + "Subscribers But Your channel have only {} subscribers.</code>"
+
+ChnlAlrdyInDTbse =  "<b>Channel is already in database Contact " + AdminUsername + " to any queries.</b>"
