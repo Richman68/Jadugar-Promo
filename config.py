902c2634d115cb8badc463b64723888a930b683b
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-10-10 21:38:33 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 class Config:
-    BOT_TOKEN = "2036153841:AAE7e6fRrv430Qoau0JsI-Mr27ElNB74hDA"
+    BOT_TOKEN = "1907012846:AAHQ_Sx0XQ0seYoKZkQKHm81mqKmM1WF_Po"
     sheetname = "promotionv4"
     sheetA = "channels"
     sheetB = "users"
+    sheetC = "fixedText"
     app = "promotionv4"
     admins = [608824855]
+    ListChannel1 = "1315390231"
+    ListChannel = "-100" + f"{ListChannel1}"
