22bbfa3111dc2a6f15f25992641416ef7930a1cb
xogamer57 <124810870+xogamer57@users.noreply.github.com>
2023-02-19 10:24:18 +0530

    update main.py

--- main.py
+++ main.py
@@ -2425,7 +2425,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://superfast-promo-zzzk.onrender.com/' + f"{TOKEN}")
+    bot.set_webhook(url='https://superfast-promo-fcui.onrender.com/' + f"{TOKEN}")
     return "!", 200
  
 if __name__ == "__main__":
