e1aa35c69250e9e8da1b8e338456cbd383185f59
Richman68 <143199730+Richman68@users.noreply.github.com>
2024-09-25 00:25:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2426,7 +2426,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://jadugar-promo53772.onrender.com/' + f"{TOKEN}")
+    bot.set_webhook(url='https://jadugar-promo-5778.onrender.com/' + f"{TOKEN}")
     return "!", 200
  
 if __name__ == "__main__":
